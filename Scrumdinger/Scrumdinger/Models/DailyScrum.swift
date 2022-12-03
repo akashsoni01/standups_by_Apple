734ccce86167b4057a1319b899b3c193897c083a
Akash Soni <akashsoni8285@gmail.com>
2022-12-03 16:52:26 +0530

    attende model added
    Section 4
    Iterate through attendees

--- Scrumdinger/Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -26,7 +26,7 @@ struct DailyScrum: Identifiable {
     let id: UUID
     
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
     
@@ -35,7 +35,7 @@ struct DailyScrum: Identifiable {
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
             self.id = id
             self.title = title
-            self.attendees = attendees
+            self.attendees = attendees.map { Attendee(name: $0) }
             self.lengthInMinutes = lengthInMinutes
             self.theme = theme
         }
@@ -48,5 +48,20 @@ extension DailyScrum {
         DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
         DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
     ]
+}
+
+
+// In DailyScrum.swift, create an extension with an inner structure named Attendee that is identifiable.
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        // Add an initializer that assigns a default value to the id property.
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
 }
 
