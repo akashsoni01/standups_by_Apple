6eb0e9cfec21907189d73c6460c01367b376126b
Akash Soni <akashsoni8285@gmail.com>
2022-12-03 16:26:42 +0530

    detail view wip

---
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Akash Soni on 03/12/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
+
