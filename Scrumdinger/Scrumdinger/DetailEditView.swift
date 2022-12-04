f34448bc58b3ba4c3faa81b3676e444326a7e281
Akash Soni <akashsoni8285@gmail.com>
2022-12-04 12:55:11 +0530

    Section 3
    Display attendees in the edit view
    In this section, you’ll display the attendees inside DetailEditView. You’ll add support for adding and deleting users.

--- Scrumdinger/Scrumdinger/DetailEditView.swift
+++ Scrumdinger/Scrumdinger/DetailEditView.swift
@@ -19,6 +19,11 @@ struct DetailEditView: View {
     // Recall that the @State property wrapper defines the source of truth for value types.
     // Declare @State properties as private so they can be accessed only within the view in which you define them.
     @State private var data = DailyScrum.Data()
+    
+    // Add a @State property named newAttendeeName.
+    // The newAttendeeName property will hold the attendee name that the user enters.
+    @State private var newAttendeeName = ""
+
 
     var body: some View {
         // Replace the Text view with a Form and add a Section with a “Meeting Info” header.
@@ -42,8 +47,46 @@ struct DetailEditView: View {
                     Text("\(Int(data.lengthInMinutes)) minutes")
                 }
             }
+            // Add a second Section with an “Attendees” header.
+            Section(header: Text("Attendees")) {
+                // Add a ForEach view, and display each attendee in a Text view.
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                // Add an onDelete modifier to remove attendees from the scrum data.
+                // The framework calls the closure you pass to onDelete when the user swipes to delete a row.
+                .onDelete { indices in
+                    data.attendees.remove(atOffsets: indices)
+                }
+                // Add an HStack containing a TextField, and pass a binding to the newAttendeeName property.
+                HStack {
+                    // The binding keeps newAttendeeName in sync with the contents of the text field. It doesn’t affect the original DailyScrum model data.
+                    TextField("New Attendee", text: $newAttendeeName)
+                    // Add a Button with an empty action and an Image label.
+                    // Before animation block
+                    /*
+                     Button(action: {}) {
+                         Image(systemName: "plus.circle.fill")
+                     }
+                     */
+                    // After animation block
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            data.attendees.append(attendee)
+                            // Set newAttendeeName to an empty string inside the animation block.
+                            // Because the text field has a binding to newAttendeeName, setting the value to the empty string also clears the contents of the text field.
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    // Disable the button if newAttendeeName is empty.
+                    .disabled(newAttendeeName.isEmpty)
+                }
+
+            }
         }
-        
     }
 }
 
